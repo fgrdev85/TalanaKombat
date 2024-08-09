8ca89cff67ad0e5aafdcc6dd7f23ffc7494b3700
fgrdev85 <133058624+fgrdev85@users.noreply.github.com>
2024-08-09 10:11:35 -0400

    se agregan comentarios

--- talanakombat.py
+++ talanakombat.py
@@ -18,7 +18,6 @@ descuentoTonyn = {
     'P': 1,
     'BLK': 0,
 }
-
 descuentoArnaldol = {
     'ArnaldolTaladoken': 2,
     'ArnaldolRemuyuken': 3,
@@ -46,7 +45,7 @@ def detectar_secuencia(buffer, movimientos):
             return nombre_movimiento
     return None
 
-
+#Detecta secuencias y define el movimiento
 def recibir_entrada(input, jugador):
     buffer.append(input)
     if jugador == 1:
@@ -64,6 +63,8 @@ entradasjugador = {
     'ArnaldolRemuyuken': 'conecta un Remuyuken',
     'ArnaldolTaladoken': 'conecta un Taladoken',
 }
+
+#Función que relata las jugadas
 def relata_jugadas(entradas_jugador, flag):
     golpe = ''
     if flag:
@@ -75,6 +76,7 @@ def relata_jugadas(entradas_jugador, flag):
         golpe = entradas_jugador[-1]
     return golpe
 
+#Permite discriminar entre un movimiento y golpes simples
 def procesa_entrada(entradas_jugador, jugador):
     buffer.append(entradas_jugador[0])
     for i in range(1,len(entradas_jugador)):
@@ -101,7 +103,7 @@ def reconocer_secuencias(lista, expresion):
         resultados.append(matches)
     return resultados
 
-
+#Preprocesamiento de listas
 def procesar_listas(lista):
     for i in range(len(lista)):
         if len(lista[i]) == 1:
@@ -116,13 +118,14 @@ def concatenar_listas(a, b):
         a[i].append(b[i][0])
     return a
 
+# Cuenta los caracteres apra definir el jugador que comienza
 def cuenta_caracteres(l):
     contador = 0
     for i in range(len(l)):
         contador = contador + len(l[i])
     return contador
 
-
+# Cuenta los caracteres apra definir el jugador que comienza
 def inicia_juego(a,b,c,d):
     inicia = 1
     l1 = cuenta_caracteres(a)
@@ -139,10 +142,12 @@ def inicia_juego(a,b,c,d):
         inicia = 2
     return inicia
 
+#Valida que sean maximo 5 movimientos
 def validar_movs(l):
     for i in range(len(l)):
         l[i] = l[i][:5]
 
+#Valida que sea 1 solo golpe
 def validar_golpes(l):
     for i in range(len(l)):
         l[i] = l[i][:1]
@@ -158,13 +163,14 @@ ruta_archivo = 'datos.json'
 # Leer el archivo JSON
 contenido_json = leer_json(ruta_archivo)
 
-
+#Extrae los movimientos y los golpes de ambos jugadores
 lista1 = contenido_json['player1']['movimientos']
 lista2 = contenido_json['player1']['golpes']
 lista3 = contenido_json['player2']['movimientos']
 lista4 = contenido_json['player2']['golpes']
 
-
+#Valida que sean maximo 5 movimientos
+#Valida que sea 1 solo golpe
 validar_movs(lista1)
 validar_golpes(lista2)
 validar_movs(lista3)
@@ -180,19 +186,21 @@ m2 = reconocer_secuencias(lista3, expresion_regular)
 # m1 significa golpes del player2
 g2 = reconocer_secuencias(lista4, expresion_regular)
 
+#Procesamiento previo de las listas
 m1 = procesar_listas(m1)
 g1 = procesar_listas(g1)
 m2 = procesar_listas(m2)
 g2 = procesar_listas(g2)
-
 l1 = concatenar_listas(m1, g1)
 l2 = concatenar_listas(m2, g2)
 
+# Define el jugador que empieza
 inicia = inicia_juego(lista1,lista2,lista3,lista4)
 puntosplayer1 = 6
 puntosplayer2 = 6
-
 largo_menor = len(l1) if len(l1) < len(l2) else len(l2)
+
+#Flujo principal escribe el relato y cambia el orden dependiendo del jugador que empieza
 for i in range(largo_menor):
     if inicia == 1:
         print(' Tonyn ', end='')
@@ -221,6 +229,7 @@ for i in range(largo_menor):
         if puntosplayer2 <= 0:
             break
 
+#Salida final define el jugador ganador
 if puntosplayer1 > puntosplayer2:
     print('Tonyn Gana la pelea y aun le queda', puntosplayer1, 'de energía')
 else:
